87caccb025338a10cdbeba6cad7416a09fc2c9ec
Ismaeilal <sq3m@hotmail.com>
2023-05-28 21:49:14 +0200

    Update master.js

--- master.js
+++ master.js
@@ -1,5 +1,4 @@
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script>
+
 $(document).ready(function() {
   // إنشاء العنصر الذي سيتم تخصيصه
   $('<div id="profile" style="width: 200px; height: 200px; background-color: #ffffff;"></div>');
@@ -36,3 +35,51 @@ $(document).ready(function() {
   });
 });
 </script>
+.profile-background {
+  width: 100%;
+  height: 200px;
+  background-color: #e1e1e1;
+  background-image: url('default-background.jpg');
+  background-size: cover;
+  background-position: center;
+}
+
+.profile-content {
+  padding: 20px;
+  /* أنماط تنسيق المحتوى في الملف الشخصي */
+}
+
+.settings {
+  margin-top: 20px;
+}
+
+.settings label {
+  display: block;
+  margin-bottom: 5px;
+}
+
+.settings input[type="color"],
+.settings input[type="file"] {
+  margin-bottom: 10px;
+}
+$(document).ready(function() {
+  // تغيير لون الخلفية
+  $('#background-color').change(function() {
+    var color = $(this).val();
+    $('.profile-background').css('background-color', color);
+  });
+
+  // تغيير صورة الخلفية
+  $('#background-image').change(function(e) {
+    var file = e.target.files[0];
+    var reader = new FileReader();
+
+    reader.onload = function(e) {
+      var imageUrl = e.target.result;
+      $('.profile-background').css('background-image', 'url(' + imageUrl + ')');
+    };
+
+    reader.readAsDataURL(file);
+  });
+});
+
